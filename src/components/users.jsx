0c32a3d343aec8a0f5919f0eb6d2411eb79000a0
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2022-07-05 21:09:22 +0300

    start creating an HTML

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import api from "../api";
-
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const Users = () => {
   // const [users, setUsers] = useState(api.fetchAll());
   // const handleDelete = (userId) => {};
-  // const renderPhrase = (number) => {};
-  return <></>;
+  const renderPhrase = (number) => {
+    return (
+      <>
+        <h2>
+          <span className="badge bg-primary m-2">С тобой сегодня затусят</span>
+        </h2>
+      </>
+    );
+  };
+  return (
+    <>
+      <div>{renderPhrase()}</div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </>
+  );
 };
 
 export default Users;
