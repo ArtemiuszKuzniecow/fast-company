0a5583c01dabd31b760b1320be6860afbea6335a
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2022-07-12 20:59:49 +0300

    keep refactoring

--- src/app/components/searchStatus.jsx
+++ src/app/components/searchStatus.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
-const SearchStatus = ({ length }) => {};
+const SearchStatus = ({ length }) => {
+  // const renderPhrase = (number) => {
+  //   let phrase;
+  //   let bgColor;
+  //   if (
+  //     (Number(number.toString()[number.toString().length - 2]) > 1 &&
+  //       Number(number.toString()[number.toString().length - 1]) >= 2 &&
+  //       Number(number.toString()[number.toString().length - 1]) <= 4) ||
+  //     (number.toString().length === 1 &&
+  //       Number(number.toString()[number.toString().length - 1]) >= 2 &&
+  //       Number(number.toString()[number.toString().length - 1]) <= 4)
+  //   ) {
+  //     phrase = number + " человека туснут с тобой сегодня";
+  //     bgColor = "bg-primary";
+  //   } else if (number === 0) {
+  //     phrase = "Никто с тобой не тусанёт";
+  //     bgColor = "bg-danger";
+  //   } else {
+  //     phrase = number + " человек туснёт с тобой сегодня";
+  //     bgColor = "bg-primary";
+  //   }
+  //   return (
+  //     <h2>
+  //       <span className={"badge m-2 " + bgColor}>{phrase}</span>
+  //     </h2>
+  //   );
+  // };
+};
 
 export default SearchStatus;
